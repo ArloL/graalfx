5b88c05a928aa44b00ac06e3860f7dbaa6afa21f
Arlo O'Keeffe <tiger@k5d.de>
2023-12-26 14:20:50 +0100

    format

--- src/main/java/io/github/arlol/graalfx/GraalFX.java
+++ src/main/java/io/github/arlol/graalfx/GraalFX.java
@@ -1,4 +1,5 @@
 package io.github.arlol.graalfx;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -7,18 +8,21 @@ import javafx.stage.Stage;
 
 public class GraalFX extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+	@Override
+	public void start(Stage stage) {
+		String javaVersion = System.getProperty("java.version");
+		String javafxVersion = System.getProperty("javafx.version");
+		Label l = new Label(
+				"Hello, JavaFX " + javafxVersion + ", running on Java "
+						+ javaVersion + "."
+		);
+		Scene scene = new Scene(new StackPane(l), 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 
 }
