26bdd091bf6dcf6d25053ba795f27625240fb329
Arlo O'Keeffe <tiger@k5d.de>
2023-12-26 15:08:40 +0100

    add version output

--- src/main/java/io/github/arlol/graalfx/Launcher.java
+++ src/main/java/io/github/arlol/graalfx/Launcher.java
@@ -3,6 +3,13 @@ package io.github.arlol.graalfx;
 public class Launcher {
 
 	public static void main(String[] args) {
+		if (args.length == 1 && "--version".equals(args[0])) {
+			Package pkg = Launcher.class.getPackage();
+			String title = pkg.getImplementationTitle();
+			String version = pkg.getImplementationVersion();
+			System.out.println(title + " version \"" + version + "\"");
+			return;
+		}
 		GraalFX.main(args);
 	}
 
