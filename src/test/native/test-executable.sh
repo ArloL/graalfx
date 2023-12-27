501834ba9c762f754567913f2b0c69a95c373753
Arlo O'Keeffe <tiger@k5d.de>
2023-12-27 12:15:47 +0100

    update path of test executable

--- src/test/native/test-executable.sh
+++ src/test/native/test-executable.sh
@@ -6,8 +6,18 @@ set -o xtrace
 
 cd "$(dirname "$0")/../../.." || exit 1
 
+OS=${2:-platform}
+if [ "${OS}" = "macos" ]; then
+    OS=darwin
+fi
+
+ARCH=$(uname -m)
+if [ "${ARCH}" = "arm64" ] && [ "${OS}" = "darwin" ]; then
+    ARCH=aarch64
+fi
+
 expectedOutput="${1:-artifact} version \"${3:-revision}\""
-executable="./target/${1:-artifact}-${2:-platform}-${3:-revision}"
+executable="./target/gluonfx/${ARCH}-${OS}/${1:-artifact}"
 actualOutput=$(${executable} --version)
 if [ "$actualOutput" != "$expectedOutput" ]; then
     echo "The output is wrong. Expected:"
